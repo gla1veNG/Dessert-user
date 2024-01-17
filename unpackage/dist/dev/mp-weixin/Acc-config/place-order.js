35b2e4880cc14e216f344ca6fd506bedf45846b1
T1R <2993078435@qq.com>
2024-01-17 15:02:03 +0800

    完成提交相同的商品到购物车

--- unpackage/dist/dev/mp-weixin/Acc-config/place-order.js
+++ unpackage/dist/dev/mp-weixin/Acc-config/place-order.js
@@ -16,8 +16,10 @@ const ORDER = common_vendor.reactive({
     //规格
     subtotal: 0,
     //总价
-    select: false
+    select: false,
     //购物车是否选中
+    SPECE_STR: ""
+    //用于查询相同规格的
   },
   exist: false,
   //判断是是否存在秒杀或者秒杀开始或结束
@@ -25,13 +27,17 @@ const ORDER = common_vendor.reactive({
   //购物车数量
 });
 const db = common_vendor.wx$1.cloud.database();
-const _ = db.command;
 const SHCART = function() {
   let subtotal = parseFloat(ORDER.order.goods_price * ORDER.order.buy_amount.toFixed(10));
   ORDER.order.subtotal = subtotal;
+  let SPECE_STR = "";
+  if (ORDER.order.specs.length > 0) {
+    ORDER.order.specs.forEach((item) => SPECE_STR += item.att_val);
+    ORDER.order.SPECE_STR = SPECE_STR;
+  }
   return new Promise(async (resolve, reject) => {
     try {
-      let res = await db.collection("sh_cart").where({ goods_id: ORDER.order.goods_id, specs: _.eq(ORDER.order.specs) }).get();
+      let res = await db.collection("sh_cart").where({ goods_id: ORDER.order.goods_id, SPECE_STR }).get();
       if (res.data.length > 0) {
         resolve("加入购物车成功");
       } else {
